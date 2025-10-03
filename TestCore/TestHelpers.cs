9342a08fef58805eb2f3afe06947f5b15a7398f2
Dev-by-robin <robinkorbee@gmail.com>
2025-10-03 12:32:41 +0200

    Geautomatiseerde tests toegevoegd. Slagen allemaal.

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -1,4 +1,5 @@
 using Grocery.Core.Helpers;
+using Grocery.Core.Models;
 
 namespace TestCore
 {
@@ -31,14 +32,49 @@ namespace TestCore
         [Test]
         public void TestPasswordHelperReturnsFalse()
         {
-            Assert.Pass(); //Zelf uitwerken
+            //Assert.Pass(); //Zelf uitwerken
+            string password = "user3";
+            string passwordHash = "notvalid123";
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
-        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
+        [TestCase("user1", "notvalid1233408")]
+        [TestCase("user3", "notvalid123")]
         public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            //Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
+        }
+
+
+
+
+        // Happy Flow
+        [Test]
+        public void CanUserSeeBoughtProductsViewTrue()
+        {
+            Client.Role userRole = Client.Role.Admin;
+            Assert.IsTrue(userRole == Client.Role.Admin);
+        }
+
+        [TestCase(Client.Role.Admin)]
+        public void CanUserSeeBoughtProductsViewTrue(Client.Role userRole)
+        {
+            Assert.IsTrue(userRole == Client.Role.Admin);
+        }
+
+        // unhappy Flow
+        [Test]
+        public void CanUserSeeBoughtProductsViewFalse()
+        {
+            Client.Role userRole = Client.Role.None;
+            Assert.IsFalse(userRole == Client.Role.Admin);
+        }
+
+        [TestCase(Client.Role.None)]
+        public void CanUserSeeBoughtProductsViewFalse(Client.Role userRole)
+        {
+            Assert.IsFalse(userRole == Client.Role.Admin);
         }
     }
 }
